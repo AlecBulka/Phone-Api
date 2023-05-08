2ef76b83248ff2ca3a2123514cc72c281a002202
= <=>
2023-05-08 17:19:59 +0000

    Display id

--- app/Http/Resources/PhoneResource.php
+++ app/Http/Resources/PhoneResource.php
@@ -15,6 +15,7 @@ class PhoneResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'brand' => $this->brand,
             'os' => $this->os,
